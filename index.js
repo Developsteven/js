b169e73fb85028caa93ad1b1c20617e17b17bc08
Developsteven <steven.perez099@gmail.com>
2020-12-04 23:19:28 -0500

    ejercicio_2

--- index.js
+++ index.js
@@ -27,18 +27,20 @@ TipoMotor(1);
 // 2
 function obtenerValorConImpuesto(a, b) {
     
-    var m = (a * b)
+    var m = (a * b);
+    var valor = (b *21 /100);
+    var valorT = (valor + a);
     console.log(m);
 
     switch (b) {
         case 1:
-            alert("El valor sin impuestos mas el 21% si el tipo de producto es 1");
+            alert("El valor sin impuestos mas el 21% si el tipo de producto es 1" + valorT);
             break;
         case 2:
-            alert("El valor sin impuestos mas el 10% si el tipo de producto es 2");
+            alert("El valor sin impuestos mas el 10% si el tipo de producto es 2" + valorT);
             break;
         case 3:
-            alet("El valor sin impuestos mas el 5% si el tipo de producto es 3" );
+            alet("El valor sin impuestos mas el 5% si el tipo de producto es 3" + valorT);
             break;
     }
 }
